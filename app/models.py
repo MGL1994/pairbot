30bc14239b889b39922071d5176a57933db6b008
MGL1994 <michaelgl1994@gmail.com>
2020-05-26 15:51:30 +0100

    Initial commit

---
+++ app/models.py
@@ -0,0 +1,16 @@
+from app import db
+
+class Pairing(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.DateTime, index=True, unique=True, nullable=False)
+    pair_one = db.Column(db.String(80), nullable=False)
+    pair_two = db.Column(db.String(80), nullable=False)
+    pair_three = db.Column(db.String(80), nullable=False)
+    pair_four = db.Column(db.String(80), nullable=False)
+    pair_five = db.Column(db.String(80), nullable=False)
+    pair_six = db.Column(db.String(80), nullable=False)
+    pair_seven = db.Column(db.String(80), nullable=False)
+    pair_eight = db.Column(db.String(80), nullable=False)
+
+    def __repr__(self):
+        return '<Date {}>'.format(self.date)
