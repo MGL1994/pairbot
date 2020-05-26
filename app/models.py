f14aeeffbd40084598bbca99de80ebd9e54579cc
MGL1994 <michaelgl1994@gmail.com>
2020-05-26 16:11:49 +0100

    Added datetime default

--- app/models.py
+++ app/models.py
@@ -1,8 +1,9 @@
+from datetime import datetime
 from app import db
 
 class Pairing(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    date = db.Column(db.DateTime, index=True, unique=True, nullable=False)
+    date = db.Column(db.DateTime, index=True, unique=True, nullable=False, default=datetime.utcnow)
     pair_one = db.Column(db.String(80), nullable=False)
     pair_two = db.Column(db.String(80), nullable=False)
     pair_three = db.Column(db.String(80), nullable=False)
